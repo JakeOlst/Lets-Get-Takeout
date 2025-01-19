7bf658619ec4065e7ac2ef4647404a12e61f5a29
JakeOlst <jstainthorpe123@gmail.com>
2025-01-19 17:48:52 +0000

    Added code for generic Shopping Bag class - constructor creates a new HashMap to store takeout items.

--- ShoppingBag.java
+++ ShoppingBag.java
@@ -1,6 +1,10 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class ShoppingBag {
-    
+public class ShoppingBag<T extends PricedItem<Integer>> {
+    private Map<T, Integer> shoppingBag;
+
+    public ShoppingBag() {
+        this.shoppingBag = new HashMap<>();
+    }
 }
