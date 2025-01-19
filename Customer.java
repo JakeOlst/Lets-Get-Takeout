ad3c1a2ef51bca479177d4b19127aff6c442d6fd
JakeOlst <jstainthorpe123@gmail.com>
2025-01-19 17:24:07 +0000

    Created class for Customer

--- Customer.java
+++ Customer.java
@@ -1,3 +1,20 @@
 public class Customer {
-    
+    private String name;
+    private int money;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMoney() {
+        return this.name;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
 }
